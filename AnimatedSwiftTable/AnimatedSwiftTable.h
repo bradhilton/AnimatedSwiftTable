50cdf6a1360cf1bae64f01b3cfb27ab7a18e982c
bradhilton <brad.hilton.nw@gmail.com>
2016-02-19 09:33:39 -0700

    Initial Commit

---
+++ AnimatedSwiftTable/AnimatedSwiftTable.h
@@ -0,0 +1,19 @@
+//
+//  AnimatedSwiftTable.h
+//  AnimatedSwiftTable
+//
+//  Created by Bradley Hilton on 2/19/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnimatedSwiftTable.
+FOUNDATION_EXPORT double AnimatedSwiftTableVersionNumber;
+
+//! Project version string for AnimatedSwiftTable.
+FOUNDATION_EXPORT const unsigned char AnimatedSwiftTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnimatedSwiftTable/PublicHeader.h>
+
+
